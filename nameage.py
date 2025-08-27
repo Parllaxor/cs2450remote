bcc2ac152d6f7db681e6bbb08688f345d6e10f89
root <root@DevsLaptop.localdomain>
2025-08-27 10:59:16 -0600

    no repeat guesses

--- nameage.py
+++ nameage.py
@@ -8,18 +8,21 @@ print(f"""Hello {name}, nice to meet you!
 I'd like to guess how old you are.""")
 
 def guess_age(name):
+    guessed_numbers = []
     x = 1
     while x > 0:
         random_age = random.randint(15, 30)
-        age_guess = input(f"Are you {random_age} years old? ")
-        if "yes" in age_guess.lower():
-            print(f"""YES! {name} is {random_age} years old!""")
-            sys.exit()
+        if random_age not in guessed_numbers:
+            age_guess = input(f"Are you {random_age} years old? ")
+            if "y" in age_guess.lower():
+                print(f"""YES! {name} is {random_age} years old!""")
+                sys.exit()
 
-        elif "no" in age_guess.lower():
-            print("""Rats.""")
+            elif "n" in age_guess.lower():
+                print("""Rats.""")
+                guessed_numbers.append(random_age)
 
-        else:
-            print("""I don't understand that, let me try guessing again.""")
+            else:
+                print("""I don't understand that, let me try guessing again.""")
 
 guess_age(name)
